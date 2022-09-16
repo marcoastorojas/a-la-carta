ddae9b2bfe5879ed7d80f6667290b36a66781e3e
cristian <cristianelmalo64@gmail.com>
2022-09-16 03:26:07 -0500

    menu service creation and configuration

--- src/app/modules/menu/services/menu.service.ts
+++ src/app/modules/menu/services/menu.service.ts
@@ -5,5 +5,14 @@ import { Injectable } from '@angular/core';
 })
 export class MenuService {
 
+  private Menu: DishApp[] = []
+  private isLoading: boolean = true
+
+  addDish(dish: DishApp) {
+    //validacion de menu (no repetido, solo 2 vegetarianos y demas, solo 4)
+  }
+  removeDish(id: number) {
+    
+  }
   constructor() { }
 }
