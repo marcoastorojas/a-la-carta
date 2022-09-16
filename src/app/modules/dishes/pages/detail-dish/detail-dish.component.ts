a4124b61544592a0a126b4070e199a44b6248d93
cristian <cristianelmalo64@gmail.com>
2022-09-16 03:36:54 -0500

    injecting service Menu

--- src/app/modules/dishes/pages/detail-dish/detail-dish.component.ts
+++ src/app/modules/dishes/pages/detail-dish/detail-dish.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuService } from 'src/app/modules/menu/services/menu.service';
 
 @Component({
   selector: 'app-detail-dish',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailDishComponent implements OnInit {
 
-  constructor() { }
+  
+  constructor(private menuService:MenuService) { }
 
   ngOnInit(): void {
   }
-
+  addDish(){
+    // this.menuService.addDish()
+  }
+  removeDish(){
+    //
+  }
 }
