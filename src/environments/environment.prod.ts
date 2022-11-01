e12611fe41c4ab20520710cf62bb578fc89d7c1a
marco <you@example.com>
2022-11-01 18:21:00 -0500

    change enviroments prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl:"https://api.spoonacular.com",
+  api_key1:"837af218d058494eaa022a2317f83988",
+  api_key2:"89975b415f604ae892240bcab3935a3b",
+  api_key3:"cb1c464d94f142c08b156c5beddade8b"
 };
